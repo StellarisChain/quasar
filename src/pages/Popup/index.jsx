f6449f72822f729b241b6c7439f090f12fbd7300
Connor <107011324+connor33341@users.noreply.github.com>
2025-08-15 13:41:41 -0400

    fixes

--- src/pages/Popup/index.jsx
+++ src/pages/Popup/index.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Buffer } from 'buffer';
 
-import PopupWrapper from './PopupWrapper';
+import Popup from './Popup';
 import './index.css';
 
 const container = document.getElementById('app-container');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 window.Buffer = Buffer; // Make Buffer globally available
-root.render(<PopupWrapper />);
+root.render(<Popup />);
