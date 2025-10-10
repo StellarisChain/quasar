5a1daf210923921f1c96106d911e57838886d8c4
Connor <107011324+connor33341@users.noreply.github.com>
2025-10-09 20:27:53 -0400

    Bump Version

--- src/components/SendModal.tsx
+++ src/components/SendModal.tsx
@@ -216,12 +216,12 @@ export const SendModal: React.FC<SendModalProps> = ({ wallet, allWallets, onClos
                 tokenData ? tokenData.find(token => token.Symbol === selectedAsset?.symbol)?.Node ?? undefined : undefined,
                 (wallet.curve ?? 'secp256k1') as CurveType
             );
-            
+
             // Generate the transaction hash if not already set
             if (result && !result.tx_hash) {
                 await result.hash();
             }
-            
+
             setTransactionHash(result?.tx_hash ?? 'n0x' + Math.random().toString(16).substring(2, 66));
             setTransactionFee(result?.fees ?? new Decimal(0));
             setIsProcessing(false);
